b34cdbf6471051448dfbb023dae7f53d5df780df
josephine-bob <josephine.robert@eleves.ec-nantes.fr>
2016-09-15 12:23:02 +0200

    header v3
    add responsive blue banner
    also checked on firefox, before it was only good for chrome

--- header.php
+++ header.php
@@ -23,38 +23,47 @@
 <div id="page" class="site">
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding" id="top-bar">
-                    <div class="mobile-part not-desktop">
-                        <img src="<?php bloginfo('template_directory'); ?>/images/toggle-menu-icon.png" alt="mobile menu">
-                    </div>
-                    <div class="sb-logo mobile-part">
-                        <img src="<?php bloginfo('template_directory'); ?>/images/soundboks-black.png" alt="soundboks logo">
-                    </div>
-                
-                    <div class="menu-bar">
-                        <div class="location-menu">
-                            <ul class="location-items">
-                                <li class="menu-item"><a>US</a></li>
-                                <li class="menu-item"><a class="selected-item">EUROPE</a></li>
-                            </ul>
-                        </div>
-
-                        <div class="pages-menu">
-                            <ul class="pages-items">
-                                <li class="menu-item"><a href="index.php" class="selected-item">SPEAKER</a></li>
-                                <li class="menu-item"><a href="index.php">SHOP</a></li>
-                                <li class="menu-item"><a href="index.php">SUPPORT</a></li>
-                            </ul>
-
-                        </div>
-                        <div id="cart-logo" class="mobile-part">
-                            <div id="full-cart"></div>
-                            <img src="<?php bloginfo('template_directory'); ?>/images/cart-full-black.png" alt="cart logo">
-                        </div>
-                    </div>
-                    
+            <div class="blue-banner not-mobile">
+                <div class="blue-message">
+                    SOUNDBOKS HAS BEEN ADDED TO YOUR CART
+                    <img class="close-banner" src="<?php bloginfo('template_directory'); ?>/images/close-little.png" alt="cross">
                 </div>
-		
+            </div>
+            <div class="site-branding" id="top-bar">
+                <div id="menu-mobile" class="mobile-part-header not-desktop">
+                    <img src="<?php bloginfo('template_directory'); ?>/images/toggle-menu-icon.svg" alt="mobile menu">
+                </div>
+                <div id="sb-logo-header" class="sb-logo mobile-part-header">
+                    <img src="<?php bloginfo('template_directory'); ?>/images/soundboks-black.png" alt="soundboks logo">
+                </div>
+                
+                <div class="menu-bar">
+                    <div class="location-menu">
+                        <ul class="location-items">
+                            <li class="menu-item"><a>US</a></li>
+                            <li class="menu-item"><a class="selected-item">EUROPE</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="pages-menu">
+                        <ul class="pages-items">
+                            <li class="menu-item"><a href="index.php" class="selected-item">SPEAKER</a></li>
+                            <li class="menu-item"><a href="index.php">SHOP</a></li>
+                            <li class="menu-item"><a href="index.php">SUPPORT</a></li>
+                        </ul>
+                    </div>
+                    <div id="cart-logo" class="mobile-part">
+                        <div id="full-cart"></div>
+                        <img src="<?php bloginfo('template_directory'); ?>/images/cart-full-black.png" alt="cart logo">
+                    </div>
+                </div>     
+            </div>
+            <div class="blue-banner not-desktop">
+                <div class="blue-message">
+                    SOUNDBOKS HAS BEEN ADDED TO YOUR CART
+                    <img class="close-banner" src="<?php bloginfo('template_directory'); ?>/images/close-little.png" alt="cross">
+                </div>
+            </div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">

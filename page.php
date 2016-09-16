3af996a938a0df16e8712d6e17af50abe62b6a81
josephine-bob <josephine.robert@eleves.ec-nantes.fr>
2016-09-16 16:22:54 +0200

    page v2
    Second part of the cart page + responsive

--- page.php
+++ page.php
@@ -15,21 +15,21 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-            <div id="top-bar-cart">
-                <div id="cart-icon">
+            <div id="top-bar-cart" class="flex-between">
+                <div id="cart-icon" class="flex-between">
                     <img src="<?php bloginfo('template_directory'); ?>/images/big-cart-icon.png" alt="cart icon">
                     <div id="cart-word">CART</div>
                 </div>
-                <div id="delivery-info" class="not-mobile">
-                    <div class="one-delivery-info">
+                <div id="delivery-info" class="not-mobile flex-between">
+                    <div class="one-delivery-info flex-between">
                         <img src="<?php bloginfo('template_directory'); ?>/images/return-icon.png" alt="return icon">
                         <div class="small-info">30 DAYS RETURN</div>
                     </div>
-                    <div class="one-delivery-info">
+                    <div class="one-delivery-info flex-between">
                         <img src="<?php bloginfo('template_directory'); ?>/images/materials.png" alt="materials">
                         <div class="small-info">2 YEARS WARRANTY</div>
                     </div>
-                    <div class="one-delivery-info">
+                    <div class="one-delivery-info flex-between">
                         <img src="<?php bloginfo('template_directory'); ?>/images/shipping-icon.png" alt="shipping icon">
                         <div class="small-info">WORLDWIDE <br/> 
                             INSTANT SHIPPING</div>
@@ -40,7 +40,7 @@ get_header(); ?>
             <div class="gray-line"></div>
             
             <div id="products-in-cart">
-                <div class="product-in-cart margin-needed">
+                <div class="product-in-cart margin-needed flex-between">
                     <div class="image-product">
                         <img src="<?php bloginfo('template_directory'); ?>/images/4.png" alt="soundboks">
                         
@@ -70,9 +70,9 @@ get_header(); ?>
                 </div>
                 <div class="price-mobile not-desktop">
                     <div class="quantity-mobile">
-                        <img class="rotate-image90" src="<?php bloginfo('template_directory'); ?>/images/rectangle-95.png" alt="plus one">
+                        <img class="rotate-image90" src="<?php bloginfo('template_directory'); ?>/images/rectangle-95.svg" alt="plus one">
                         <div class="quantity">3</div>
-                        <img class="rotate-image270" src="<?php bloginfo('template_directory'); ?>/images/rectangle-95.png" alt="minus one">
+                        <img class="rotate-image270" src="<?php bloginfo('template_directory'); ?>/images/rectangle-95.svg" alt="minus one">
                     </div>
                     <div class="product-column">    
                         €699.00
@@ -82,7 +82,7 @@ get_header(); ?>
                     </div>
                 </div>
                     <div class="gray-line"></div>
-                <div class="product-in-cart margin-needed">
+                <div class="product-in-cart margin-needed flex-between">
                     <div class="image-product">
                         <img src="<?php bloginfo('template_directory'); ?>/images/4.png" alt="soundboks">
                         
@@ -112,15 +112,80 @@ get_header(); ?>
                 </div>
                 <div class="price-mobile not-desktop">
                     <div class="quantity-mobile">
-                        <img class="rotate-image90" src="<?php bloginfo('template_directory'); ?>/images/rectangle-95.png" alt="plus one">
+                        <img class="rotate-image90" src="<?php bloginfo('template_directory'); ?>/images/rectangle-95.svg" alt="plus one">
                         <div class="quantity">3</div>
-                        <img class="rotate-image270" src="<?php bloginfo('template_directory'); ?>/images/rectangle-95.png" alt="minus one">
+                        <img class="rotate-image270" src="<?php bloginfo('template_directory'); ?>/images/rectangle-95.svg" alt="minus one">
                     </div>
                     <div class="product-column">    
                         €699.00
                     </div>
                     <div class="product-column">
                         TOTAL €2,796.00
+                    </div>
+                </div>
+            </div>
+            
+            <div class="gray-line"></div>
+            
+            <div id="order-info" class="flex-between">
+                <div id="discount-column">
+                    <form id="discount-form">
+                        <input type="text" name="discount" id="discount-input" placeholder="DISCOUNT CODE">
+                        <input type="submit" name="submit" id="submit-discount" value="APPLY DISCOUNT">
+                    </form>
+                    <div class="discount-code flex-between">
+                        <div class="green-circle"></div>
+                        <div>
+                            <span class="code-ex">SBJULY16</span>: $200 discount for purchase of a SOUNDBOKS <br/>
+                                or other related products above $600
+                        </div>
+                    </div>
+                    <div class="discount-code flex-between not-mobile">
+                        <div class="green-circle"></div>
+                        <div>
+                            <span class="code-ex">SBJULY16</span>: $200 discount for purchase of a SOUNDBOKS <br/>
+                                or other related products above $600
+                        </div>
+                    </div>
+                </div>
+                <div id="price-column">
+                    <div class="price-line flex-between">
+                        <div>
+                            TOTAL
+                        </div>
+                        <div>
+                            $18,997.00
+                        </div>
+                    </div>
+                    <div class="price-line flex-between">
+                        <div>
+                            APPLIED DISCOUNT
+                        </div>
+                        <div>
+                            -$3,459.00
+                        </div>
+                    </div>
+                    <div class="price-line flex-between">
+                        <div>
+                            ORDER SUBTOTAL
+                        </div>
+                        <div>
+                            $17,997.00
+                        </div>
+                    </div>
+                    <div id="update-price" class="flex-between">
+                        <div class="align-center">
+                            <img src="<?php bloginfo('template_directory'); ?>/images/update-icon.png" alt="round arrows">
+                        </div>
+                        <div id="update-cart" class="align-center">UPDATE CART</div>
+                        <div id="checkout" class="align-center">CHECKOUT</div>
+                    </div>
+                    <div id="vat-price" class="payment-feature">INCl. 15% VAT 230,- DKK</div>
+                    <div class="gray-line not-desktop"></div>
+                    <div id="pay-per-month" class="payment-feature">
+                        <div id="pay-little">PAY AS LITTLE AS $25/MONTH</div>
+                        <div id="apply-check">Apply & check out in seconds</div>
+                        <div id="learn-more">Learn More</div>
                     </div>
                 </div>
             </div>
